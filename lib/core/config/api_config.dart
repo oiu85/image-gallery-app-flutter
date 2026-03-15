7d429ce45b2c3f6c408515dc0a6f97a26d6c6dc3
Abdullah <love14144mn@gmail.com>
2026-03-15 18:29:19 +0300

    *. Create the api config file

---
+++ lib/core/config/api_config.dart
@@ -0,0 +1,10 @@
+//*  API Configuration
+class ApiConfig {
+  /// Base URL for all API requests
+  static const String baseUrl = 'https://xxx.com';
+
+
+  /// TODO: Add more endpoints as needed
+  // static const String register = '$baseUrl/auth/register';
+  // static const String refreshToken = '$baseUrl/auth/refresh';
+}
